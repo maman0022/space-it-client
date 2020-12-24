fdc9524d3fd44c1964f02a1fd067f9089445535d
maman0022 <66834086+maman0022@users.noreply.github.com>
2020-12-24 04:17:56 -0600

    changed title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -43,7 +43,7 @@ class Header extends Component {
       <header>
         <h1>
           <Link to='/'>
-            SpACE IT!
+            spACE IT!
           </Link>
         </h1>
         {TokenService.hasAuthToken()
